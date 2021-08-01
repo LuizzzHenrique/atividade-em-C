66ecc7af37ca1a383baa2a88ee30840a19af3009
LuizzzHenrique <86574307+LuizzzHenrique@users.noreply.github.com>
2021-08-01 13:25:47 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{float a,b;
+
+for( ; ; ){
+
+printf("informe o valor em km percorridos:\n ");
+scanf("%f",&a);
+
+printf("informe a quantidade de gasolina:\n");
+scanf ("%f",&b);
+
+    float divisaum = a/b;
+printf ("o resultado eh: %f\n\n",divisaum);}
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
